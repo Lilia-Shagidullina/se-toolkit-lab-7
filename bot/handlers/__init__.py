18265624ede2a4d46fc61b1df6e821075f4fbc3c
Lilia-Shagidullina <l.shagidullina@innopolis.university>
2026-03-21 13:40:54 +0300

    Update handlers __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,1 +1,8 @@
 """Command handlers for the LMS bot."""
+
+from .start import handle_start
+from .help import handle_help
+from .health import handle_health
+from .labs import handle_labs
+
+__all__ = ["handle_start", "handle_help", "handle_health", "handle_labs"]
